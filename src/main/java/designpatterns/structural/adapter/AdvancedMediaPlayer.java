6dab91b662f7cb4a546c1fe87fff6a6647b8ac17
Manan <gmanan3222@gmail.com>
2025-05-29 13:11:14 +0530

    Adapter design pattern

---
+++ src/main/java/designpatterns/structural/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package designpatterns.structural.adapter;
+
+public interface AdvancedMediaPlayer {
+    void playVlc(String fileName);
+    void playMp4(String fileName);
+}
---
+++ src/main/java/designpatterns/structural/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package designpatterns.structural.adapter;
+
+public interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
